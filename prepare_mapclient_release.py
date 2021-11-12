d02dbb4c2052ec5a8575ce401f080e38a9b603e0
hsorby <h.sorby@auckland.ac.nz>
2021-11-13 05:16:36 +1300

    Add debug statement.

--- prepare_mapclient_release.py
+++ prepare_mapclient_release.py
@@ -22,47 +22,47 @@ def main():
 
     # Always install opencmiss.zinc, numpy, and scipy
     result = subprocess.run(["pip", "install", "opencmiss.zinc", "numpy", "scipy"])
-    print(' == result install extras:', result.returncode)
+    print(' == result install extras:', result.returncode, flush=True)
 
     mapclient_url = "https://github.com/MusculoskeletalAtlasProject/mapclient"
     result = subprocess.run(["git", "-c", "advice.detachedHead=false", "clone", "--depth", "1", mapclient_url, "-b", args.mapclient_release])
-    print(' == result git:', result.returncode)
+    print(' == result git:', result.returncode, flush=True)
 
     result = subprocess.run(["pip", "install", "-e", 'mapclient/src'])
-    print(' == result install:', result.returncode)
+    print(' == result install:', result.returncode, flush=True)
 
     if args.plugins is not None and os.path.isfile(args.plugins):
         result = subprocess.run([sys.executable, "prepare_mapclient_plugins.py", args.plugins])
-        print(' == result plugins preparation:', result.returncode)
+        print(' == result plugins preparation:', result.returncode, flush=True)
 
     working_env = os.environ.copy()
 
     if args.workflows is not None and os.path.isfile(args.workflows):
         result = subprocess.run([sys.executable, "prepare_mapclient_workflows.py", args.workflows])
-        print(' == result workflow preparation:', result.returncode)
+        print(' == result workflow preparation:', result.returncode, flush=True)
 
         working_env["INTERNAL_WORKFLOWS_ZIP"] = os.path.abspath('internal_workflows.zip')
 
     current_directory = os.getcwd()
     os.chdir("mapclient/res/pyinstaller/")
     print('=======================')
-    print(sys.executable)
+    print(sys.executable, flush=True)
     # subprocess.run(['pip.exe', 'list'])
     # print(working_env)
     print([sys.executable, "create_application.py", variant])
     result = subprocess.run([sys.executable, "create_application.py", variant], env=working_env)
-    print(' == result application creation:', result.returncode)
+    print(' == result application creation:', result.returncode, flush=True)
     os.chdir(current_directory)
 
     if platform.system() == "Windows":
         os.chdir("mapclient/res/win")
         result = subprocess.run([sys.executable, "create_installer.py", args.mapclient_release, variant], env=working_env)
-        print(' == result create installer:', result.returncode)
+        print(' == result create installer:', result.returncode, flush=True)
         os.chdir(current_directory)
     elif platform.system() == "Darwin":
         os.chdir("mapclient/res/macos")
         result = subprocess.run(["/bin/bash", "create_installer.sh", args.mapclient_release, f"-{variant}"], env=working_env)
-        print(' == result create installer:', result.returncode)
+        print(' == result create installer:', result.returncode, flush=True)
         os.chdir(current_directory)
 
 
