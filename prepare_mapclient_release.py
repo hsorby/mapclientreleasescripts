7405b2632febd1c01f28f9ffd1a2be8d80400570
hsorby <h.sorby@auckland.ac.nz>
2021-11-12 23:42:48 +1300

    Make the release script more flexible.

--- prepare_mapclient_release.py
+++ prepare_mapclient_release.py
@@ -2,23 +2,23 @@
 import argparse
 import os
 import os.path
-import re
+import platform
 import subprocess
 import sys
 
 
 def main():
     parser = argparse.ArgumentParser(prog="release_preparation")
-    parser.add_argument("mapclient_release", help="A tag from mapclient codebase")
-    parser.add_argument("plugin_listing", help="A file of plugins to prepare")
-    parser.add_argument("workflow_listing", help="A file of workflows to prepare")
+    parser.add_argument("mapclient_release", help="tag from mapclient codebase")
+    parser.add_argument('-p', '--plugins', nargs=1, type=argparse.FileType('r'), help='input plugins list file')
+    parser.add_argument('-w', '--workflows', nargs=1, type=argparse.FileType('r'), help='input workflows list file')
+    parser.add_argument('-v', '--variant', nargs=1, help='variant label for this build')
     args = parser.parse_args()
 
-    if not os.path.exists(args.plugin_listing):
-        sys.exit(1)
+    variant = args.variant if args.variant is not None else ''
 
-    if not os.path.exists(args.workflow_listing):
-        sys.exit(2)
+    result = subprocess.run(["pip", "install", "-U", 'pip'])
+    print(' == result install:', result.returncode)
 
     mapclient_url = "https://github.com/MusculoskeletalAtlasProject/mapclient"
     result = subprocess.run(["git", "-c", "advice.detachedHead=false", "clone", "--depth", "1", mapclient_url, "-b", args.mapclient_release])
@@ -27,20 +27,34 @@ def main():
     result = subprocess.run(["pip", "install", "-e", 'mapclient/src'])
     print(' == result install:', result.returncode)
 
-    result = subprocess.run([sys.executable, "prepare_mapclient_plugins.py", args.plugin_listing])
-    print(' == result plugins preparation:', result.returncode)
-
-    result = subprocess.run([sys.executable, "prepare_mapclient_workflows.py", args.workflow_listing])
-    print(' == result workflow preparation:', result.returncode)
+    if args.plugins is not None:
+        result = subprocess.run([sys.executable, "prepare_mapclient_plugins.py", args.plugins])
+        print(' == result plugins preparation:', result.returncode)
 
     working_env = os.environ.copy()
-    working_env["INTERNAL_WORKFLOWS_ZIP"] = os.path.abspath('internal_workflows.zip')
+
+    if args.workflows is not None:
+        result = subprocess.run([sys.executable, "prepare_mapclient_workflows.py", args.workflows])
+        print(' == result workflow preparation:', result.returncode)
+
+        working_env["INTERNAL_WORKFLOWS_ZIP"] = os.path.abspath('internal_workflows.zip')
 
     current_directory = os.getcwd()
     os.chdir("mapclient/res/pyinstaller/")
-    result = subprocess.run([sys.executable, "create_application.py"], env=working_env)
+    result = subprocess.run([sys.executable, "create_application.py", variant], env=working_env)
     print(' == result application creation:', result.returncode)
     os.chdir(current_directory)
+
+    if platform.system() == "Windows":
+        os.chdir("mapclient/res/win")
+        result = subprocess.run([sys.executable, "create_installer.py", args.mapclient_release, variant], env=working_env)
+        print(' == result application creation:', result.returncode)
+        os.chdir(current_directory)
+    elif platform.system() == "Darwin":
+        os.chdir("mapclient/res/macos")
+        result = subprocess.run(["/bin/bash", "create_installer.sh", args.mapclient_release, variant], env=working_env)
+        print(' == result application creation:', result.returncode)
+        os.chdir(current_directory)
 
 
 if __name__ == "__main__":
