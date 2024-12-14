27a79892049bd6de7c3f70f0318373807489d52b
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-12-14 17:00:17 +1300

    Add retries to Darwin create installer command.

--- create_mapclient_installer.py
+++ create_mapclient_installer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import argparse
-import glob
 import os
 import os.path
 import platform
-import shutil
 import subprocess
 import sys
-
+import time
 
 MAP_CLIENT_REPO = "mapclient"
 
@@ -45,6 +43,13 @@ def main():
         os.chdir(os.path.join(local_mapclient, "res", "macos"))
         result = subprocess.run(["/bin/bash", "create_installer.sh", release_name, f"-{variant}" if variant else ''], env=working_env)
         print(' == result create installer:', result.returncode, flush=True)
+        retries = [1, 3, 5]
+        retry_index = 0
+        while result.returncode:
+            time.sleep(retries[retry_index])
+            result = subprocess.run(["/bin/bash", "create_installer.sh", release_name, f"-{variant}" if variant else ''], env=working_env)
+            print(' == result create installer:', result.returncode, flush=True)
+
         os.chdir(current_directory)
 
     if result.returncode:
